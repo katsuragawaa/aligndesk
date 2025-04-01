c2ca60e5b42e6fc55f823c8497277b91431ea3d1
katsuragawaa <andre.katsuragawa@gmail.com>
2025-04-01 18:38:22 -0300

    feat: implement header component and refactor layout structure

---
+++ app/components/header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full">
+      <nav className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+        <Link href="/" className="flex items-center">
+          <span className="after:bg-primary relative text-xl font-bold after:absolute after:right-0 after:bottom-0 after:h-0.5 after:w-0 after:transition-all hover:after:w-full">
+            AlignDesk
+          </span>
+        </Link>
+        <div className="flex items-center space-x-8">
+          <Link
+            href="/jobs"
+            className="after:bg-primary hover:text-primary relative text-sm font-normal transition-colors after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all hover:after:w-full"
+          >
+            Jobs
+          </Link>
+          <Link
+            href="/companies"
+            className="after:bg-primary hover:text-primary relative text-sm font-normal transition-colors after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all hover:after:w-full"
+          >
+            Companies
+          </Link>
+          <Link
+            href="/about"
+            className="after:bg-primary hover:text-primary relative text-sm font-normal transition-colors after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all hover:after:w-full"
+          >
+            About
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
+import Header from "./components/header";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.className} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
