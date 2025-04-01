1bbfa8259f6a9d4fec59a56b0a074ea0eab28faf
katsuragawaa <andre.katsuragawa@gmail.com>
2025-04-01 19:14:12 -0300

    feat: replace main content with job search section and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,106 +1,21 @@
 import Image from "next/image";
-import Header from "./components/header";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="grid min-h-[calc(100vh-4rem)] grid-rows-[1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-        <main className="row-start-1 flex flex-col items-center gap-[32px] sm:items-start">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-            <li className="mb-2 tracking-[-.01em]">
-              Get started by editing{" "}
-              <code className="bg-foreground/5 rounded px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold">
-                app/page.tsx
-              </code>
-              .
-            </li>
-            <li className="tracking-[-.01em]">
-              Save and see your changes instantly.
-            </li>
-          </ol>
-
-          <div className="flex flex-col items-center gap-4 sm:flex-row">
-            <a
-              className="bg-foreground text-background hover:bg-foreground/90 flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors sm:h-12 sm:w-auto sm:px-5 sm:text-base"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              className="border-foreground/10 hover:bg-foreground/5 flex h-10 w-full items-center justify-center rounded-full border border-solid px-4 text-sm font-medium transition-colors hover:border-transparent sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px]"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read our docs
-            </a>
-          </div>
-        </main>
-        <footer className="row-start-2 flex flex-wrap items-center justify-center gap-[24px]">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
-      </div>
+      <section className="grid min-h-[calc(100vh-4rem)] items-center">
+        <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 className="flex w-full flex-col gap-2 text-4xl font-bold tracking-tight sm:text-9xl">
+            <span className="pr-40 text-right">FIND THE JOB</span>
+            <span className="text-left">THAT FITS YOUR</span>
+            <span className="text-right">TIME ZONE</span>
+          </h1>
+          <p className="text-muted-foreground max-w-2xl text-lg sm:text-xl">
+            Streamline your workspace alignment and boost productivity with our
+            intuitive desk management solution.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
