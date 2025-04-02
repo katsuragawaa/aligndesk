f056281147ab35cc87ae9289c1882b8ef46743fc
katsuragawaa <andre.katsuragawa@gmail.com>
2025-04-02 13:20:18 -0300

    fix: change div to main for semantic HTML structure in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <main className="min-h-screen">
       <Hero />
-    </div>
+    </main>
   );
 }
