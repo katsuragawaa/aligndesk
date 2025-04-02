9a1505f7fb04a6c8332cdf968af76e04140c3e4b
katsuragawaa <andre.katsuragawa@gmail.com>
2025-04-02 11:37:03 -0300

    fix: update layout dimensions and text sizing for improved readability on home page

--- app/page.tsx
+++ app/page.tsx
@@ -48,13 +48,13 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <section className="grid min-h-[calc(100vh-4rem)] items-start">
-        <div className="mx-auto w-full max-w-7xl px-4 py-32 sm:px-6 lg:px-8">
+        <div className="mx-auto w-full max-w-3xl px-4 py-32 sm:px-6 lg:px-8">
           <div className="flex flex-col gap-2">
-            <h1 className="flex w-full flex-col gap-2 text-4xl font-bold tracking-tight sm:text-9xl">
-              <span className="pr-40 text-right">FIND THE JOB</span>
+            <h1 className="flex w-full flex-col gap-2 text-4xl font-bold tracking-tight sm:text-7xl">
+              <span className="pr-16 text-right">FIND THE JOB</span>
               <span className="text-left">THAT FITS YOUR</span>
-              <div className="flex items-center gap-20">
-                <div className="text-base font-normal tracking-normal">
+              <div className="flex items-center gap-8">
+                <div className="text-sm font-normal tracking-normal">
                   <p>
                     Discover flexible work-from-home{" "}
                     <span className="after:bg-primary text-primary relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full">
